62752819c26cd5af41877a22a775214940a7f777
wandinhawright <wwanda.wwright@gmail.com>
2025-09-20 14:19:25 -0300

    cria classes do model e adiciona os models ao admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Usuario, Pedido, Produto
 # Register your models here.
+class PedidoInline(admin.TabularInline):  # ou StackedInline para visual diferente
+    model = Pedido
+    extra = 1  # quantos "pedidos vazios" aparecem para cadastrar rápido
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    inlines = [PedidoInline]
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    pass
--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Usuario(AbstractUser):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=15)
+    endereco = models.CharField(max_length=255)
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    valor= models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='produtos/')
+    marca = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+
+class Pedido(models.Model):
+    produtos = models.ManyToManyField(Produto)
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(
+        Usuario,  # ou User se estiver usando o padrão
+        on_delete=models.CASCADE,
+        related_name="pedidos"
+    )
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    situacao = models.CharField(default='PENDENTE',choices=[
+        ('PENDENTE', 'Pendente'),
+        ('FEITO', 'Feito'),
+        ('FINALIZADO', 'Finalizado')
+    ])
